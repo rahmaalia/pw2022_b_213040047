f9d6f1c66ffa12c0416095905639dc2fae31a4c1
rahmaalia <rahmaaliaputri27@gmail.com>
2022-06-10 11:28:45 +0700

    upload sql terbaru

--- tubes/penjualan.sql
+++ tubes/penjualan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Jun 2022 pada 18.16
--- Versi server: 10.4.21-MariaDB
--- Versi PHP: 8.0.12
+-- Generation Time: Jun 10, 2022 at 05:32 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -33,7 +33,7 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
@@ -44,7 +44,7 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
@@ -58,7 +58,7 @@ CREATE TABLE `produk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `produk`
+-- Dumping data for table `produk`
 --
 
 INSERT INTO `produk` (`id_produk`, `nama_produk`, `stok`, `harga`, `gambar`, `kategori_id`, `keterangan`) VALUES
@@ -72,12 +72,14 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `stok`, `harga`, `gambar`, `ka
 (18, 'Exsport Weekender Rucksack - Dark Green', 12, 200000, '627e2d4931151.jpg', 1, 'tas yang mengusung tema perkemahan ini memiliki bahan yang cukup kuat dan muat untuk banyak barang dan sangat cocok untuk kalian yang memiliki jiwa berpetualang seperti si unyil dan si otan'),
 (19, 'Exsport Mochila Poly Sling Bag - Purple', 1, 55000, '627e2d8c2cf29.jpg', 3, 'slingbag yang simpel dan muat untuk banyak barang, seeprti dompet, hape, dan beberapa perintilan kecil lainnya. tas ini juga sangat cocok sebagai ootd kalian karena sangat modis sekali bukan dengan warna yang cantik'),
 (20, 'Exsport Essential Sling Bag - Light Blue', 10, 500000, '627e2dcd822b0.jpg', 3, 'slingbag yang sangat simpel namun tetap terlihat cantik, muat untuk hape iphone 13 pro-max ataupun hp besar lainnya, slingbag inijuga dilengkapi dengan tali yang sangat ringkas sehingga membawakan kesan imut pada anda'),
-(21, 'Handheld Laptop Tote Bag - Salem', 11, 600000, '627e2e1051156.jpg', 2, 'totebag berwarna pink yang berfungsi sebagai laptop handled, memiliki bahan yang ringan sehingga tidak membuat tas semakin berat. tas ini juga muat untuk membawa banyak barang dan sangat cocok dipakai untuk ke kampus ato ke kantor');
+(21, 'Handheld Laptop Tote Bag - Salem', 11, 600000, '629f497ed79df.jpg', 2, 'totebag berwarna pink yang berfungsi sebagai laptop handled, memiliki bahan yang ringan sehingga tidak membuat tas semakin berat. tas ini juga muat untuk membawa banyak barang dan sangat cocok dipakai untuk ke kampus ato ke kantor'),
+(30, 'haii nama saya rahma', 500, 60, '62a09dafad502.jpg', 2, 'sdfsf'),
+(31, 'asdad', 456, 567, '62a1f7b4b67e0.jpg', 2, 'ryrty');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -87,66 +89,67 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 (1, 'admin', '$2y$10$3buFveYnOkxjMaPdqncK0OjA2L12i0QmkVSBrW3G.WBgjgF7781Cm'),
 (2, 'rahma', '$2y$10$7nSQQl/tHoamVp1.qkQdJOuP/NoDP7DlgHOnPjClK5AUr5/Y/rq5S'),
 (5, 'raisya', '$2y$10$KYTPkgw.TvBFD3FHNqe4nuzxTNKBuFtarsU0M19LP/bLI/H5aDFQO'),
-(6, 'ica', '$2y$10$0CNl97SJjTqHiaLUtZ6LHeyoM3pPwrGGxhH7Rt13Y8XYLKYvwCFS2');
+(6, 'ica', '$2y$10$0CNl97SJjTqHiaLUtZ6LHeyoM3pPwrGGxhH7Rt13Y8XYLKYvwCFS2'),
+(7, 'gibran', '$2y$10$ddeJGb1gv0L2vhLtxn1mOuY.fjG2J8g0wSscVjBvnvXfznngDN532');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `kategori`
+-- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`),
   ADD KEY `kategori_id` (`kategori_id`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `kategori`
+-- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `produk`
+-- Constraints for table `produk`
 --
 ALTER TABLE `produk`
   ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id_kategori`);
